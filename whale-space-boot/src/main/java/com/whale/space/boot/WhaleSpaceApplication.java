ed641da997a6f9eb6cd3845c75e697772e1ef570
Lcxuan <1986057637@qq.com>
2026-03-06 16:31:23 +0800

    init: 初始化项目

---
+++ whale-space-boot/src/main/java/com/whale/space/boot/WhaleSpaceApplication.java
@@ -0,0 +1,12 @@
+package com.whale.space.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhaleSpaceApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(WhaleSpaceApplication.class, args);
+  }
+}
+
